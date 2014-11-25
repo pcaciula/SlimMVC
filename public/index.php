4844e26b6c1871a3a702d024d7ac1dfa6fc09e78
pcaciula <pcaciula@gmail.com>
2014-11-25 14:59:21 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 error_reporting(E_ALL | E_STRICT);
-date_default_timezone_set('America/Fortaleza');
+date_default_timezone_set('America/New York');
 
 define('BASEDIR', dirname(__DIR__)); // root directory
 define('APPDIR', BASEDIR . '/src'); // app directory
